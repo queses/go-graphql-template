5d39e5416fd3a53482d55e7241eabbc3f816f9bf
Max Kobozev <quesesr@gmail.com>
2022-12-27 13:10:11 +0300

    Init repo

---
+++ src/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	graph2 "github.com/queses/go-template/src/graph"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/queses/go-template/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph2.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
